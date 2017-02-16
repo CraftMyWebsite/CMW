e2d7dc10de8929f067101710046732c85b023cf7
Sprik07 <inconnuedu07@hotmail.fr>
2017-02-16 12:37:10 +0100

    MiniUpdate
    Ajout:
    - Affichage du nom de grade dans le profil du joueur.
    - Suppression des commentaires d'un ticket si le ticket en question est
    supprimé.
    - Suppression des statistiques de la boutique si l'offre en question est
    supprimé.

--- controleur/profil/index.php
+++ controleur/profil/index.php
@@ -1,39 +1,42 @@
 <?php
 
-	include('controleur/profil/serveur.php');
-	
-	include('modele/joueur/donneesJoueur.class.php');
+include('controleur/profil/serveur.php');
 
-	$joueurDonnees = new JoueurDonnees($bddConnection, $_GET['profil']);
-	$joueurDonnees = $joueurDonnees->getTableauDonnees();
-	
-	if(empty($joueurDonnees))
-	{
-		$joueurDonnees['rang'] = 0;
-		$joueurDonnees['email'] = 'inconnu';
-		$joueurDonnees['skype'] = 'inconnu';
-		$joueurDonnees['age'] = 'inconnu';
-	}
-	
-	if(empty($joueurDonnees['skype']))
-		$joueurDonnees['skype'] = 'inconnu';
-	if(empty($joueurDonnees['age']))
-		$joueurDonnees['age'] = 'inconnu';
+include('modele/joueur/donneesJoueur.class.php');
 
-	
-		
-	if($joueurDonnees['rang'] == 1)
-		$gradeSite = 'Créateur';
-	elseif($joueurDonnees['rang'] == 2) 
-		$gradeSite = 'Administrateur';
-	elseif($joueurDonnees['rang'] == 3)
-		$gradeSite = 'Modérateur';
-	elseif($joueurDonnees['rang'] == 4)
-		$gradeSite = 'Développeur';
-	elseif($joueurDonnees['rang'] == 5)
-		$gradeSite = 'Buildeur';
-	else
+$joueurDonnees = new JoueurDonnees($bddConnection, $_GET['profil']);
+$joueurDonnees = $joueurDonnees->getTableauDonnees();
+
+if(empty($joueurDonnees))
+{
+	$joueurDonnees['rang'] = 0;
+	$joueurDonnees['email'] = 'inconnu';
+	$joueurDonnees['skype'] = 'inconnu';
+	$joueurDonnees['age'] = '??';
+}
+
+if(empty($joueurDonnees['skype']))
+	$joueurDonnees['skype'] = 'inconnu';
+if(empty($joueurDonnees['age']))
+	$joueurDonnees['age'] = '??';
+if(empty($joueurDonnees['tokens']))
+	$joueurDonnees['tokens'] = '0';
+
+
+
+if($joueurDonnees['rang'] == 0) {
+	$gradeSite = 'Joueur';
+} elseif($joueurDonnees['rang'] == 1) {
+	$gradeSite = "<font color='red'>Créateur";
+} elseif(fopen('./modele/grades/'.$joueurDonnees['rang'].'.yml', 'r')) {
+	$openGradeSite = new Lire('./modele/grades/'.$joueurDonnees['rang'].'.yml');
+	$readGradeSite = $openGradeSite->GetTableau();
+	$gradeSite = $readGradeSite['Grade'];
+	if(empty($readGradeSite['Grade']))
 		$gradeSite = 'Joueur';
-	
-	include('theme/' .$_Serveur_['General']['theme']. '/pages/profil.php');
+} else {
+	$gradeSite = 'Joueur';
+}
+
+include('theme/' .$_Serveur_['General']['theme']. '/pages/profil.php');
 ?>
