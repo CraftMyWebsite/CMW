7dbe0874a29817415e901ac5056f05abf255c63c
Florentlife <curvalle.florent@gmail.com>
2020-07-25 17:08:01 +0200

    Pull Guedesite

--- Build/include/phpmailer/MailSender.php
+++ Build/include/phpmailer/MailSender.php
@@ -22,7 +22,7 @@ class MailSender
 				$mail->Password = htmlspecialchars($arrayData['SMTP']['Password']);
 				$mail->SMTPSecure = $arrayData['SMTP']['Protocol'];
 				$mail->Port = $arrayData['SMTP']['Port'];
-			
+				$mail->Timeout = 5;
 				$mail->From = $arrayData['SMTP']['From'];
 				$mail->FromName = $arrayData['General']['name'];
 				$mail->addAddress($to);   
