a0c406588e77bf521b06fc2af755a2b93cd6ba44
BadiiiX <59808543+BadiiiiX@users.noreply.github.com>
2020-08-23 02:41:43 +0200

    Update structure page
    New structure :P

--- Build/theme/default/maintenance.php
+++ Build/theme/default/maintenance.php
@@ -1,6 +1,6 @@
 <?php
-include('controleur/maintenance.php');
-require('include/version.php');
+include ('controleur/maintenance.php');
+require ('include/version.php');
 ?>
 
 <!DOCTYPE html>
@@ -9,50 +9,53 @@ require('include/version.php');
 <head>
 
 	<title>
-		<?= $_Serveur_['General']['name'] . " | MAINTENANCE " ?>
+		<?=$_Serveur_['General']['name'] . " | MAINTENANCE " ?>
 	</title>
 
 	<!-- Meta -->
 	<meta charset="UTF-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-	<meta name="theme-color" content="<?= $_Serveur_["color"]["theme"]["main"]; ?>">
-	<meta name="msapplication-navbutton-color" content="<?= $_Serveur_["color"]["theme"]["main"]; ?>">
-	<meta name="apple-mobile-web-app-statut-bar-style" content="<?= $_Serveur_["color"]["theme"]["main"]; ?>">
-	<meta name="apple-mobile-web-app-capable" content="<?= $_Serveur_["color"]["theme"]["main"]; ?>">
+	<meta name="theme-color" content="<?=$_Serveur_["color"]["theme"]["main"]; ?>">
+	<meta name="msapplication-navbutton-color" content="<?=$_Serveur_["color"]["theme"]["main"]; ?>">
+	<meta name="apple-mobile-web-app-statut-bar-style" content="<?=$_Serveur_["color"]["theme"]["main"]; ?>">
+	<meta name="apple-mobile-web-app-capable" content="<?=$_Serveur_["color"]["theme"]["main"]; ?>">
 
-	<meta property="og:title" content="<?= $_Serveur_['General']['name'] ?>">
+	<meta property="og:title" content="<?=$_Serveur_['General']['name'] ?>">
 	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://<?= $_SERVER["SERVER_NAME"] ?>">
-	<meta property="og:image" content="https://<?= $_SERVER["SERVER_NAME"] ?>/favicon.ico">
-	<meta property="og:image:alt" content="<?= $_Serveur_['General']['description'] ?>">
-	<meta property="og:description" content="<?= $_Serveur_['General']['description'] ?>">
-	<meta property="og:site_name" content="<?= $_Serveur_['General']['name'] ?>" />
+	<meta property="og:url" content="https://<?=$_SERVER["SERVER_NAME"] ?>">
+	<meta property="og:image" content="https://<?=$_SERVER["SERVER_NAME"] ?>/favicon.ico">
+	<meta property="og:image:alt" content="<?=$_Serveur_['General']['description'] ?>">
+	<meta property="og:description" content="<?=$_Serveur_['General']['description'] ?>">
+	<meta property="og:site_name" content="<?=$_Serveur_['General']['name'] ?>" />
 
-	<meta name="twitter:title" content="<?= $_Serveur_['General']['name'] ?>">
-	<meta name="twitter:description" content="<?= $_Serveur_['General']['description'] ?>">
-	<meta name="twitter:image" content="https://<?= $_SERVER["SERVER_NAME"] ?>/favicon.ico">
+	<meta name="twitter:title" content="<?=$_Serveur_['General']['name'] ?>">
+	<meta name="twitter:description" content="<?=$_Serveur_['General']['description'] ?>">
+	<meta name="twitter:image" content="https://<?=$_SERVER["SERVER_NAME"] ?>/favicon.ico">
 
-	<meta name="author" content="CraftMyWebsite, TheTueurCiTy, <?= $_Serveur_['General']['name']; ?>" />
+	<meta name="author" content="CraftMyWebsite, TheTueurCiTy, <?=$_Serveur_['General']['name']; ?>" />
 
 	<!-- CSS links -->
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
-	<link rel="stylesheet" href="theme/<?= $_Serveur_['General']['theme']; ?>/assets/css/custom.css">
+	<link rel="stylesheet" href="theme/<?=$_Serveur_['General']['theme']; ?>/assets/css/custom.css">
+	<style>
+
+	</style>
 
 </head>
 
 <body>
 	<?php
-	//Verif Version
-	include("./include/version.php");
-	include("./include/version_distant.php");
-	?>
+//Verif Version
+include ("./include/version.php");
+include ("./include/version_distant.php");
+?>
 
 	<?php
-	$maintenanceOn = true;
-	include('theme/' . $_Serveur_['General']['theme'] . '/entete.php'); //Header included
-	tempMess(); ?>
+$maintenanceOn = true;
+include ('theme/' . $_Serveur_['General']['theme'] . '/entete.php'); //Header included
+tempMess(); ?>
 
 
 	<!-- Contenue de la page -->
@@ -70,111 +73,117 @@ require('include/version.php');
 				</div>
 			</div>
 
-			<?php if (!empty($donnees['dateFin'])) :
-				if ($donnees['dateFin'] != 0 && $donnees['dateFin'] <= time()) {
-					$req = $bddConnection->prepare('UPDATE cmw_maintenance SET maintenanceEtat = :maintenanceEtat WHERE maintenanceId = :maintenanceId');
-					$req->execute(array('maintenanceEtat' => 0, 'maintenanceId' => $donnees['maintenanceId']));
-					header("Location: /");
-				} ?>
-				<div class="row">
-					<!-- Temps restannt -->
-					<div class="card col-10 my-4 mx-auto">
-						<div id="clockdiv">
-							<div class="card-header">
-								<h4>Temps Restant :</h4>
-							</div>
-							<div class="card-body">
-								<div class="row">
-									<div class="col">
-										<div class="content-clock">
-											<span class="bloc-clock days h5"></span>
-											<div>Jours</div>
-										</div>
-									</div>
-									<div class="col">
-										<div class="content-clock">
-											<span class="bloc-clock hours h5"></span>
-											<div>Heures</div>
-										</div>
-									</div>
-									<div class="col">
-										<div class="content-clock">
-											<span class="bloc-clock minutes h5"></span>
-											<div>Minutes</div>
-										</div>
-									</div>
-									<div class="col">
-										<div class="content-clock">
-											<span class="bloc-clock seconds h5"></span>
-											<div>Secondes</div>
-										</div>
-									</div>
+			<div class="row">
+				<?php if (!empty($donnees['dateFin'])):
+    if ($donnees['dateFin'] != 0 && $donnees['dateFin'] <= time())
+    {
+        $req = $bddConnection->prepare('UPDATE cmw_maintenance SET maintenanceEtat = :maintenanceEtat WHERE maintenanceId = :maintenanceId');
+        $req->execute(array(
+            'maintenanceEtat' => 0,
+            'maintenanceId' => $donnees['maintenanceId']
+        ));
+        header("Location: /");
+    } ?>
+
+					<div class="col-sm-10 col-md-9 col-lg-7 mx-auto mt-5">
+						<div class="rounded bg-lightest text-white shadow p-5 text-center mb-5">
+							<h3 class="mb-4 font-weight-bold text-uppercase">Temps restant :</h3>
+							<h5>Voici le temps restant avant le retour de votre site !</h5>
+
+							<div id="clockdiv" class="countdown-circles d-flex flex-wrap justify-content-center pt-4">
+
+								<div class="content-clock">
+									<span class="bloc-clock days h5"></span>
+									<div>Jours</div>
 								</div>
-							</div>
-							<div class="card-footer">
-								<small>Merci pour votre patience !</small>
+								<div class="content-clock">
+									<span class="bloc-clock hours h5"></span>
+									<div>Heures</div>
+								</div>
+								<div class="content-clock">
+									<span class="bloc-clock minutes h5"></span>
+									<div>Minutes</div>
+								</div>
+								<div class="content-clock">
+									<span class="bloc-clock seconds h5"></span>
+									<div>Secondes</div>
+								</div>
+
 							</div>
 						</div>
 					</div>
-				</div>
-			<?php endif; ?>
 
-			<div class="row">
+				<?php
+endif; ?>
+
 				<!-- Connexion -->
-				<div class="col-9 mx-auto mt-3">
+				<div class="<?=(!empty($donnees['dateFin'])) ? "col-sm-9 col-md-8 col-lg-5 mt-3 mx-md-auto mx-sm-auto" : "col-8 mx-auto mt-3" ?>">
+
 					<div class="card">
+
 						<div class="card-header">
 							<h3 class="text-center m-0">Connexion</h3>
 						</div>
+
 						<div class="card-body">
-							<h5><?= $donnees['maintenanceMsgAdmin']; ?></h5>
+							<h5><?=$donnees['maintenanceMsgAdmin']; ?></h5>
 						</div>
-						<?php if(Permission::getInstance()->verifPerm("PermsPanel", "maintenance", "actions", "connexionAdmin")) { ?>
+
+						<?php if (Permission::getInstance()->verifPerm("PermsPanel", "maintenance", "actions", "connexionAdmin"))
+{ ?>
+
 							<div class="card-footer">
 								<a href="index.php" class="btn btn-main w-100">Aller sur votre site</a>
 							</div>
+
 						<?php
-					} elseif(Permission::getInstance()->verifPerm("connect")) { ?>
+}
+elseif (Permission::getInstance()
+    ->verifPerm("connect"))
+{ ?>
 							<div class="card-footer">
 								<a class="btn btn-main w-100">Vous n'avez pas la permission d'accéder au site pendant la maintenance ! </a>
 							</div>
-					<?php }	else { ?>
-						<div class="card-footer">
-							<a data-toggle="modal" data-target="#ConnectionSlide" class="btn btn-main w-100">Se connecter</a>
-						</div>
-					<?php } ?>
+						<?php
+}
+else
+{ ?>
+							<div class="card-footer">
+								<a data-toggle="modal" data-target="#ConnectionSlide" class="btn btn-main w-100">Se connecter</a>
+							</div>
+						<?php
+} ?>
 					</div>
+
+					<?php if (!$donnees['inscription'] && !Permission::getInstance()->verifPerm('connect'))
+{ ?>
+						<!-- Inscription -->
+						<div class="card mt-3">
+							<div class="card-header">
+								<h3 class="text-center m-0">Inscription</h3>
+							</div>
+							<div class="card-body">
+								<h5><?=$donnees['maintenanceMsgInscr']; ?></h5>
+							</div>
+							<div class="card-footer">
+								<a data-toggle="modal" data-target="#InscriptionSlide" class="btn btn-main w-100">S'inscrire</a>
+							</div>
+						</div>
+
+					<?php
+} ?>
+					
 				</div>
 			</div>
-			<div class="row">
-				<?php if($donnees['inscription'] && !Permission::getInstance()->verifPerm('connect')) { ?>
-				<!-- Inscription -->
-				<div class="col-9 mx-auto mt-3">
-					<div class="card">
-						<div class="card-header">
-							<h3 class="text-center m-0">Inscription</h3>
-						</div>
-						<div class="card-body">
-							<h5><?= $donnees['maintenanceMsgInscr']; ?></h5>
-						</div>
-						<div class="card-footer">
-							<a data-toggle="modal" data-target="#InscriptionSlide" class="btn btn-main w-100">S'inscrire</a>
-						</div>
-					</div>
-				</div>
-				<?php } ?>
-			</div>
-
-
-		</div>
 	</section>
 
 
 
 
-	<?php include('theme/' . $_Serveur_['General']['theme'] . '/formulaires.php'); //Forms included 
-	include('theme/' . $_Serveur_['General']['theme'] . '/pied.php');  //Footer included 
-	?>
+	<?php include ('theme/' . $_Serveur_['General']['theme'] . '/formulaires.php'); //Forms included
+include ('theme/' . $_Serveur_['General']['theme'] . '/pied.php'); //Footer included
+
+?>
 
 
 
@@ -226,7 +235,7 @@ require('include/version.php');
 			var timeinterval = setInterval(updateClock, 1000);
 		}
 
-		var deadline = new Date(Date.parse(new Date()) + <?= ($donnees["dateFin"] - time()) ?> * 1000);
+		var deadline = new Date(Date.parse(new Date()) + <?=($donnees["dateFin"] - time()) ?> * 1000);
 		initializeClock('clockdiv', deadline);
 	</script>
 </body>
