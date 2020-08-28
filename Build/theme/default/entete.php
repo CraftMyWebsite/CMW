cd23e8c8a6a40430f11da4fbbdf0683e34f94753
Florentlife <curvalle.florent@gmail.com>
2020-08-28 12:25:15 +0200

    Simplification code HTML

--- Build/theme/default/entete.php
+++ Build/theme/default/entete.php
@@ -1,40 +1,4 @@
-<?php
-//Vérification d'alerte ^^
-
-if (Permission::getInstance()->verifPerm("connect")) :
-
-    $req_topic = $bddConnection->prepare('SELECT cmw_forum_topic_followed.pseudo, vu, cmw_forum_post.last_answer AS last_answer_pseudo 
-    FROM cmw_forum_topic_followed
-    INNER JOIN cmw_forum_post WHERE id_topic = cmw_forum_post.id AND cmw_forum_topic_followed.pseudo = :pseudo');
-    $req_topic->execute(array(
-        'pseudo' => $_Joueur_['pseudo']
-    ));
-
-    $alerte = 0;
-
-    while ($td = $req_topic->fetch(PDO::FETCH_ASSOC)) {
-        if ($td['pseudo'] != $td['last_answer_pseudo'] and $td['last_answer_pseudo'] != NULL and $td['vu'] == 0) {
-            $alerte++;
-        }
-    }
-
-    $req_answer = $bddConnection->prepare('SELECT vu
-    FROM cmw_forum_like INNER JOIN cmw_forum_answer WHERE id_answer = cmw_forum_answer.id
-    AND cmw_forum_like.pseudo != :pseudo AND cmw_forum_answer.pseudo = :pseudo AND type = 2');
-    $req_answer->execute(array(
-        'pseudo' => $_Joueur_['pseudo'],
-    ));
-
-    while ($answer_liked = $req_answer->fetch(PDO::FETCH_ASSOC)) {
-        if ($answer_liked['vu'] == 0) {
-            $alerte++;
-        }
-    }
-
-endif;
-
-?>
-
+<?php require_once('theme/'. $_Serveur_['General']['theme'].'/assets/php/alerts.php'); ?>
 <!-- Header -->
 <header>
     <div class="header-content" style="background: linear-gradient(to bottom, rgba(77, 77, 77, 0.52), rgba(68, 68, 68, 0.73)), url('theme/upload/slider/<?= $_Accueil_['Slider']['image']; ?>') center;">
