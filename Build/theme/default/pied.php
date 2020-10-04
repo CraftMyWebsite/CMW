ac933efa6b3a8305d31407fc08e65d2d281cf10f
BadiiiX <59808543+BadiiiiX@users.noreply.github.com>
2020-10-04 02:33:27 +0200

    Adjust Footer for About & Social

--- Build/theme/default/pied.php
+++ Build/theme/default/pied.php
@@ -7,36 +7,37 @@ $_Theme_ = $_Theme_->GetTableau();
         <div class="container-fluid col-12 mt-3">
             <div class="row">
                 <div class="col-md-12 col-sm-12 col-lg-7">
+                    <?php if(isset($_Theme_['Pied']['about']) && !empty(trim($_Theme_['Pied']['about']))) : ?>
                     <div class="about-title h4">
                         À Propos
                     </div>
                     <div class="about-content">
                         <?= $_Theme_['Pied']['about']; ?>
                     </div>
+                    <?php endif; ?>
                 </div>
-                <div class="col-md-12 col-sm-12 col-lg-5 ml-auto">
-                    <div class="social-title h4">
-                        Nos Réseaux
+
+                <?php if (isset($_Theme_['Pied']['social']) && !empty($_Theme_['Pied']['social'])) : ?>
+                    <div class="col-md-12 col-sm-12 col-lg-5 ml-auto">
+                        <div class="social-title h4">
+                            Nos Réseaux
+                        </div>
+                        <div class="social-content">
+
+                             <?php foreach ($_Theme_['Pied']['social'] as $value) : ?>
+
+                                <a href="<?= $value['link'] ?>" class="col-12 discord-social ml-3">
+                                    <span class="social-logo">
+                                        <?= $value['icon'] ?>
+                                    </span>
+                                    <span class="social-text"> <?= $value['message'] ?> </span>
+                                </a>
+
+                            <?php endforeach; ?>
+
+                        </div>
                     </div>
-                    <div class="social-content">
-                        <a href="<?= $_Theme_['Pied']['facebook']; ?>" class="col-12 discord-social ml-3">
-                            <span class="fab fa-facebook social-logo"></span>
-                            <span class="social-text"> Rejoignez-nous sur Facebook !</span>
-                        </a>
-                        <a href="<?= $_Theme_['Pied']['youtube']; ?>" class="col-12 discord-social ml-3">
-                            <span class="fab fa-youtube social-logo"></span>
-                            <span class="social-text"> Rejoignez-nous sur Youtube !</span>
-                        </a>
-                        <a href="<?= $_Theme_['Pied']['discord']; ?>" class="col-12 discord-social ml-3">
-                            <span class="fab fa-discord social-logo"></span>
-                            <span class="social-text"> Rejoignez-nous sur Discord !</span>
-                        </a>
-                        <a href="<?= $_Theme_['Pied']['twitter']; ?>" class="col-12 discord-social ml-3">
-                            <span class="fab fa-twitter social-logo"></span>
-                            <span class="social-text"> Rejoignez-nous sur Twitter !</span>
-                        </a>
-                    </div>
-                </div>
+                <?php endif; ?>
             </div>
         </div>
     </div>
@@ -48,9 +49,6 @@ $_Theme_ = $_Theme_->GetTableau();
                         Tous droits réservés, site créé pour le serveur <?= $_Serveur_['General']['name']; ?> <br />
                         <small><a href="https://craftmywebsite.fr" target="_blank">CraftMyWebsite.fr</a>#<?= $versioncms; ?></small>
                     </div>
-                </div>
-                <div class="col text-right">
-                    <div class="theme">Thème défaut fait par <i style="color: red;" class="fas fa-heart"></i> par BadiiiX</div>
                 </div>
             </div>
         </div>
