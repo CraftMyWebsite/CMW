41d2a493c29abbd09ecb5db61c25bf5cad9fdda5
Teyir <60007549+Teyir@users.noreply.github.com>
2021-02-04 12:11:22 +0100

    Ajouts du keywords, ajouts de plusieurs meta pour améliorer le seo du thème "default"

--- theme/default/index.php
+++ theme/default/index.php
@@ -38,6 +38,7 @@ $themeColor = ($_Theme_['Main']['theme']['choosed-theme'] === 1) ? 'light': 'dar
     <meta name="apple-mobile-web-app-statut-bar-style" content="<?= $_Serveur_["color"]["theme"]["main"]; ?>">
     <meta name="apple-mobile-web-app-capable" content="<?= $_Serveur_["color"]["theme"]["main"]; ?>">
 
+    <!-- SEO -->
     <meta property="og:title" content="<?= $_Serveur_['General']['name'] ?>">
     <meta property="og:type" content="website" />
     <meta property="og:url" content="https://<?= $_SERVER["SERVER_NAME"] ?>">
@@ -51,6 +52,15 @@ $themeColor = ($_Theme_['Main']['theme']['choosed-theme'] === 1) ? 'light': 'dar
     <meta name="twitter:image" content="https://<?= $_SERVER["SERVER_NAME"] ?>/favicon.ico">
 
     <meta name="author" content="CraftMyWebsite, TheTueurCiTy, <?= $_Serveur_['General']['name']; ?>" />
+    <meta name="publisher" content="<?= $_SERVER["SERVER_NAME"] ?>"/>
+    <meta name="description" content="<?= $_Serveur_['General']['description'] ?>">
+    <meta name="keywords" content="<?= $_Serveur_['General']['keywords'] ?>">
+
+    <meta name="copyright" content="CraftMyWebsite, <?= $_Serveur_['General']['name']; ?>"/>
+
+    <meta name="robots" content="follow, index, all">
+    <meta name="google" content="notranslate">
+
 
     <!-- CSS links -->
     <link rel="stylesheet" href="theme/<?= $_Serveur_['General']['theme']; ?>/assets/css/bootstrap.min.css">
@@ -65,60 +75,60 @@ $themeColor = ($_Theme_['Main']['theme']['choosed-theme'] === 1) ? 'light': 'dar
 </head>
 
 <body>
-    <script type="text/javascript">var _Jetons_ = "<?=$_Serveur_['General']['moneyName'];?>";</script>
-    <?php
-    //Verif Version
-    include("./include/version.php");
-    include("./include/version_distant.php");
+<script type="text/javascript">var _Jetons_ = "<?=$_Serveur_['General']['moneyName'];?>";</script>
+<?php
+//Verif Version
+include("./include/version.php");
+include("./include/version_distant.php");
 
-    if ($versioncms != $versioncmsrelease && Permission::getInstance()->verifPerm('PermsPanel', 'update', 'showPage')) : ?>
+if ($versioncms != $versioncmsrelease && Permission::getInstance()->verifPerm('PermsPanel', 'update', 'showPage')) : ?>
 
-        <div class=" mb-0 rounded-0 text-center alert alert-main bg-lightest alert-dismissible text-shadow-none fade show sticky-top" role="alert">
-            <h5 class="m-0">
-                Une mise à jour est disponible <strong>(<a href="https://craftmywebsite.fr/telecharger" target="_blank" class="alert-link"><?= $versioncmsrelease ?></a>)</strong> !
-                <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="color: var(--base-color);">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </h5>
-        </div>
+    <div class=" mb-0 rounded-0 text-center alert alert-main bg-lightest alert-dismissible text-shadow-none fade show sticky-top" role="alert">
+        <h5 class="m-0">
+            Une mise à jour est disponible <strong>(<a href="https://craftmywebsite.fr/telecharger" target="_blank" class="alert-link"><?= $versioncmsrelease ?></a>)</strong> !
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close" style="color: var(--base-color);">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </h5>
+    </div>
 
-    <?php endif; ?>
+<?php endif; ?>
 
-    <?php if (Permission::getInstance()->verifPerm("connect")) /* --> */ setcookie('pseudo', $_Joueur_['pseudo'], time() + 86400, null, null, false, true);
+<?php if (Permission::getInstance()->verifPerm("connect")) /* --> */ setcookie('pseudo', $_Joueur_['pseudo'], time() + 86400, null, null, false, true);
 
-    include('theme/' . $_Serveur_['General']['theme'] . '/entete.php'); //Header included
-    tempMess(); ?>
+include('theme/' . $_Serveur_['General']['theme'] . '/entete.php'); //Header included
+tempMess(); ?>
 
-    <?php
-    //Verif Installation Folder is deleted
-    if (is_dir("installation")) {
-        include('theme/' . $_Serveur_['General']['theme'] . '/pages/fichier_installation.php');
-    } else {
-        include('controleur/page.php'); //Page included
-    }
-    include('theme/' . $_Serveur_['General']['theme'] . '/pied.php');  //Footer included
-    include('theme/' . $_Serveur_['General']['theme'] . '/formulaires.php'); //Forms included
+<?php
+//Verif Installation Folder is deleted
+if (is_dir("installation")) {
+    include('theme/' . $_Serveur_['General']['theme'] . '/pages/fichier_installation.php');
+} else {
+    include('controleur/page.php'); //Page included
+}
+include('theme/' . $_Serveur_['General']['theme'] . '/pied.php');  //Footer included
+include('theme/' . $_Serveur_['General']['theme'] . '/formulaires.php'); //Forms included
+?>
+
+<div id="divScroll" class="btn btn-main" onclick="goToTop()"><i class="fa fa-arrow-up" aria-hidden="true"></i></div>
+
+
+<!-- Librairies Essential -->
+<script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/jquery.min.js"></script>
+<script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/popper.min.js"></script>
+<script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+
+<!-- Scripts -->
+<script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/zxcvbn.js"></script>
+<script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/custom.js"></script>
+<?php include "theme/" . $_Serveur_['General']['theme'] . "/assets/php/ckeditorManager.php"; ?>
+<script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets//js/toastr.min.js"></script>
+<?php include "theme/" . $_Serveur_['General']['theme'] . "/assets/php/custom.php"; ?>
+<?php if ($_Serveur_['Payement']['dedipass']) : //API DEDIPASS
     ?>
-
-    <div id="divScroll" class="btn btn-main" onclick="goToTop()"><i class="fa fa-arrow-up" aria-hidden="true"></i></div>
-
-
-    <!-- Librairies Essential -->
-    <script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/jquery.min.js"></script>
-    <script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/popper.min.js"></script>
-    <script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/bootstrap.min.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
-
-    <!-- Scripts -->
-    <script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/zxcvbn.js"></script>
-    <script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets/js/custom.js"></script>
-    <?php include "theme/" . $_Serveur_['General']['theme'] . "/assets/php/ckeditorManager.php"; ?>
-    <script src="theme/<?= $_Serveur_['General']['theme']; ?>/assets//js/toastr.min.js"></script>
-    <?php include "theme/" . $_Serveur_['General']['theme'] . "/assets/php/custom.php"; ?>
-    <?php if ($_Serveur_['Payement']['dedipass']) : //API DEDIPASS 
-    ?>
-        <script src="//api.dedipass.com/v1/pay.js"></script>
-    <?php endif; ?>
+    <script src="//api.dedipass.com/v1/pay.js"></script>
+<?php endif; ?>
 
 </body>
 
