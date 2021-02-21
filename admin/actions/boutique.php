a98e8abcbb78b19bf0c7975eee57af3a1a5e7771
Teyir <60007549+Teyir@users.noreply.github.com>
2021-02-21 11:48:10 +0100

    Panel admin -> Changer la devis monétaire (PayPal)

--- admin/actions/boutique.php
+++ admin/actions/boutique.php
@@ -2,7 +2,7 @@
 if($_Permission_->verifPerm('PermsPanel', 'shop', 'parametres')) {
 
     $_Serveur_['General']['moneyName'] = $_POST['moneyName'];
-
+    $_Serveur_['Payement']['currency'] = $_POST['currency'];
 
     $ecriture = new Ecrire('modele/config/config.yml', $_Serveur_);
 
