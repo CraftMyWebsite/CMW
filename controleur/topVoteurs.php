f419c80856e67397bc81134910c4aa2d7783994a
Hugo MATHIEU <44479509+guedesite@users.noreply.github.com>
2021-03-11 19:03:11 +0100

    fix historique vote

--- controleur/topVoteurs.php
+++ controleur/topVoteurs.php
@@ -12,7 +12,7 @@ for($i = 0; $donneesVotes = $topVotes->fetch(PDO::FETCH_ASSOC); $i++)
 $req_vote = $bddConnection->prepare('SELECT id, lien, titre FROM cmw_votes_config WHERE serveur = :serveur');
 $count_req = $bddConnection->prepare('SELECT COUNT(id) as count FROM cmw_votes_config WHERE serveur = :serveur');
 if(isset($_Serveur_['vote']['oldDisplay'])) {
-	$oldvote_req = $bddConnection->query('SELECT pseudo, nbre_votes FROM cmw_votes WHERE 1 ORDER BY nbre_votes DESC');
+	$oldvote_req = $bddConnection->query('SELECT pseudo, nbre_votes FROM cmw_votes WHERE isOld=1 group by pseudo ORDER BY nbre_votes DESC');
 }
 
 ?>
