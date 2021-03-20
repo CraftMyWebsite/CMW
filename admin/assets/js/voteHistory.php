98cb9148e44ef24a2ec001c05362913443ab028c
Hugo MATHIEU <44479509+guedesite@users.noreply.github.com>
2021-03-20 16:29:23 +0100

    Ajout du nombre total de vote
    Le nombre se change lorsque l'on supprime un vote

--- admin/assets/js/voteHistory.php
+++ admin/assets/js/voteHistory.php
@@ -127,7 +127,7 @@ async function showAll(allPlayer) {
                 all += '<td>'+allPlayer[i].nbre_votes+'</td>';
                 all += '<td>Dernier le '+allPlayer[i].date_dernier2+'</td>';
                 all += '<td>sur '+allPlayer[i].site+'</td>';
-                <?php if($_Permission_->verifPerm('PermsPanel', 'vote', 'voteHistory', 'actions', 'removeVote')) { ?> all += '<td><button onclick="sendDirectPost(\'admin.php?action=suppVoteHistory&pseudo='+allPlayer[i].pseudo+'\', function(data) { if(data) { hide(\'histo'+i+'\'); hide(\'hiisto'+i+'\')}});" class="input-disabled btn-sm btn btn-danger">Supprimer</button></td>'; <?php } ?>
+                <?php if($_Permission_->verifPerm('PermsPanel', 'vote', 'voteHistory', 'actions', 'removeVote')) { ?> all += '<td><button onclick="get(\'countall\').innerText = parseInt(get(\'countall\').innerText)-'+allPlayer[i].nbre_votes+';sendDirectPost(\'admin.php?action=suppVoteHistory&pseudo='+allPlayer[i].pseudo+'\', function(data) { if(data) { hide(\'histo'+i+'\'); hide(\'hiisto'+i+'\')}});" class="input-disabled btn-sm btn btn-danger">Supprimer</button></td>'; <?php } ?>
             all += '</tr>';
 
             let nb = Object.entries(allPlayer[i].all).length;
