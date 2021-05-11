35d0ec2c550c89e21858d4b3b0bc444df4fb49f1
LoicBrlt <loguardian@hotmail.com>
2021-05-11 19:58:02 +0200

    [FIX] Errors on CHMOD values for installation + added CHMOD verif on install folder

--- installation/app/plugins/chmod.php
+++ installation/app/plugins/chmod.php
@@ -1,149 +1,163 @@
 <?php
 function VerifieChmod() {
+    $err = null;
+    $errDossier = null;
+    $errInstall = null;
 
-    $dirR[0] = 'app/data/install.yml';
-    $dirR[1] = '../modele/config/accueil.yml';
-    $dirR[2] = '../modele/config/config.yml';
-    $dirR[3] = '../modele/config/configMenu.yml';
-    $dirR[4] = '../modele/config/configWidgets.yml';
+    $dirInstall[0] = 'app/data/install.yml';
+    for ($i = 0; $i < count($dirInstall); $i++) {
+        $chmodi[$i] = substr(sprintf('%o', fileperms($dirInstall[$i])), -3);
+
+        $chmodInstallU[$i]['pr'] = substr($chmodi[$i], -3, 1);
+        $chmodInstallU[$i]['gr'] = substr($chmodi[$i], -2, 1);
+        $chmodInstallU[$i]['pu'] = substr($chmodi[$i], -1, 1);
+
+        if ($chmodInstallU[$i]['pr'] < 7 or $chmodInstallU[$i]['gr'] < 7 or $chmodInstallU[$i]['pu'] < 5) {
+            if ($errInstall != null)
+                $errInstall = $errInstall . ';' . (string)$i;
+            else
+                $errInstall = (string)$i;
+        }
+    }
+    if ($errInstall != null) {
+        $errInstall = explode(';', $errInstall);
+        for ($i = 0; $i < count($errInstall); $i++) {
+            $return['chmodInstall'][$i] = $chmodi[$errInstall[$i]];
+            $return['dirInstall'][$i] = $dirInstall[$errInstall[$i]];
+        }
+    }
+
+    $dirR[0] = '../modele/config/accueil.yml';
+    $dirR[1] = '../modele/config/config.yml';
+    $dirR[2] = '../modele/config/configMenu.yml';
+    $dirR[3] = '../modele/config/configWidgets.yml';
+    $dirR[4] = '../modele/config/configStats.yml';
     $dirR[5] = '../modele/.htpasswd';
     $dirR[6] = '../controleur/.htpasswd';
     $dirR[7] = '../admin/actions/.htpasswd';
-
-
-    $dir[0] = 'installation/app/data/install.yml';
-    $dir[1] = 'modele/config/accueil.yml';
-    $dir[2] = 'modele/config/config.yml';
-    $dir[3] = 'modele/config/configMenu.yml';
-    $dir[4] = 'modele/config/configWidgets.yml';
-    $dir[5] = 'modele/.htpasswd';
-    $dir[6] = 'controleur/.htpasswd';
-    $dir[7] = 'admin/actions/.htpasswd';
-
-    $dirDossier[0] = array('../theme/upload/', '../theme/upload/navRap/', '../theme/upload/panel/');
-    $dirDossier[1] = array('../utilisateurs/');
-
-    $err = null;
-    for($i = 0; $i < count($dir); $i++)
-    {
+    for ($i = 0; $i < count($dirR); $i++) {
         $chmod[$i] = substr(sprintf('%o', fileperms($dirR[$i])), -3);
 
         $chmodu[$i]['pr'] = substr($chmod[$i], -3, 1);
         $chmodu[$i]['gr'] = substr($chmod[$i], -2, 1);
         $chmodu[$i]['pu'] = substr($chmod[$i], -1, 1);
-        if($chmodu[$i]['pr'] < 6 OR $chmodu[$i]['gr'] < 4 OR $chmodu[$i]['pu'] < 4)
-        {
-            if($err != null)        
-                $err = $err . ';' . (string) $i;
+
+        if ($chmodu[$i]['pr'] < 7 or $chmodu[$i]['gr'] < 5 or $chmodu[$i]['pu'] < 5) {
+            if ($err != null)
+                $err = $err . ';' . (string)$i;
             else
-                $err = (string) $i; 
+                $err = (string)$i;
         }
     }
-    
-    $errDossier = null;
-    for($i = 0; $i < count($dirDossier); $i++)
-    {
-        $compte[$i] = false;
-        for($j = 0; $j < count($dirDossier[$i]); $j++)
-        {
-            $chmodDossier[$i][$j] = substr(sprintf('%o', fileperms($dirDossier[$i][$j])), -3);
-
-            $chmodDossierU[$i][$j]['pr'] = substr($chmodDossier[$i][$j], -3, 1);
-            $chmodDossierU[$i][$j]['gr'] = substr($chmodDossier[$i][$j], -2, 1);
-            $chmodDossierU[$i][$j]['pu'] = substr($chmodDossier[$i][$j], -1, 1);
-            if(($chmodDossierU[$i][$j]['pr'] < 6 OR $chmodDossierU[$i][$j]['gr'] < 4 OR $chmodDossierU[$i][$j]['pu'] < 4) AND !$compte[$i])
-            {
-                if($errDossier != null)        
-                    $errDossier = $errDossier . ';' . (string) $i;
-                else
-                    $errDossier = (string) $i; 
-                $compte[$i] = true;
-            }
-        }
-    }
-
-    if($err != null)
-    {
+    if ($err != null) {
         $err = explode(';', $err);
-        for($i = 0; $i < count($err); $i++) 
-        {    
+        for ($i = 0; $i < count($err); $i++) {
             $return['chmod'][$i] = $chmod[$err[$i]];
-            $return['dir'][$i] = $dir[$err[$i]];
+            $return['dir'][$i] = $dirR[$err[$i]];
         }
     }
 
-    if($errDossier != null)
-    {
-        $errDossier = explode(';', $errDossier);
-        for($i = 0; $i < count($errDossier); $i++) 
-        {    
-            $return['chmodDossier'][$i] = 'inférieur à 755 sur certains fichiers';
-            $return['dirDossier'][$i] = $dirDossier[$errDossier[$i]][0];
+    $dirDossier[0] = '../theme/upload/';
+    $dirDossier[1] = '../theme/upload/navRap/';
+    $dirDossier[2] = '../theme/upload/panel/';
+    $dirDossier[3] = '../utilisateurs/';
+    for ($i = 0; $i < count($dirDossier); $i++) {
+        $chmodDossier[$i] = substr(sprintf('%o', fileperms($dirDossier[$i])), -3);
+
+        $chmodDossierU[$i]['pr'] = substr($chmodDossier[$i], -3, 1);
+        $chmodDossierU[$i]['gr'] = substr($chmodDossier[$i], -2, 1);
+        $chmodDossierU[$i]['pu'] = substr($chmodDossier[$i], -1, 1);
+
+        if ($chmodDossierU[$i]['pr'] < 7 or $chmodDossierU[$i]['gr'] < 7 or $chmodDossierU[$i]['pu'] < 7) {
+            if ($errDossier != null)
+                $errDossier = $errDossier . ';' . (string)$i;
+            else
+                $errDossier = (string)$i;
         }
     }
-    if($err == null AND $errDossier == null)
+    if ($errDossier != null) {
+        $errDossier = explode(';', $errDossier);
+        for ($i = 0; $i < count($errDossier); $i++) {
+            $return['chmodDossier'][$i] = 'inférieur à 777 sur certains dossiers';
+            $return['dirDossier'][$i] = $dirDossier[$errDossier[$i]];
+        }
+    }
+
+
+    if ($err == null and $errDossier == null and $errInstall == null)
         return null;
     else
         return $return;
 }
 
-function DrawChmod($return)
-{
-    
-    if($return['chmodDossier'] != null OR $return['chmod'] != null){ ?>
-            <div class="pt-3">
-        <div class="alert alert-danger">
-            Veuillez modifier les chmod des dossiers/fichiers pour procéder à l'installation
+function DrawChmod($return) {
+
+    if ($return['chmodDossier'] != null or $return['chmod'] != null or $return['chmodInstall'] != null) { ?>
+        <div class="pt-3">
+            <div class="alert alert-danger">
+                Veuillez modifier les chmod des dossiers/fichiers pour procéder à l'installation
+            </div>
         </div>
-    </div>
-    <div class="block border shadow bg-texture" style="border-radius: 2% !important;">
+        <div class="block border shadow bg-texture" style="border-radius: 2% !important;">
 
-        <div class="row p-5">
-            <div class="col-md-12">
+            <div class="row p-5">
+                <div class="col-md-12">
 
 
-                <table class="table table-hover">
-                    <thead>
+                    <table class="table table-hover">
+                        <thead>
                         <tr>
-                        <th scope="col">Fichier/Dossier</th>
-                        <th scope="col">Actuel</th>
-                        <th scope="col">Requis</th>
+                            <th scope="col">Fichier/Dossier</th>
+                            <th scope="col">Actuel</th>
+                            <th scope="col">Requis</th>
                         </tr>
-                    </thead>
+                        </thead>
 
-                    <tbody>
-                    <?php if(isset($return['chmodDossier'])) {
-                    for($i = 0; $i < count($return['chmodDossier']); $i++) { ?>
+                        <tbody>
+                        <?php if (isset($return['chmodInstall'])) {
+                        for ($i = 0; $i < count($return['chmodInstall']); $i++) { ?>
                         <tr>
-                            <td><?php echo $return['dirDossier'][$i]; ?></td>
-                            <td><?php echo $return['chmodDossier'][$i]; ?></td>
-                            <td>777</td>
+                            <td><?php echo $return['dirInstall'][$i]; ?></td>
+                            <td><?php echo $return['chmodInstall'][$i]; ?></td>
+                            <td>775</td>
                         </tr>
-                        <?php } 
-                    }
-                    
-                    if(isset($return['chmod'])) {
-                    for($i = 0; $i < count($return['chmod']); $i++) { ?>
-                        <tr>
-                            <td><?php echo $return['dir'][$i]; ?></td>
-                            <td><?php echo $return['chmod'][$i]; ?></td>
-                            <td>755</td>
-                        </tr>
-                        <?php } 
-                    }?>
-                    </tbody>
-                </table>
-                <div class="row">
-                    <div class="col-md-12">
-                        <a onclick="ajax_chmod()" class="btn btn-primary text-white btn-block minecrafter">Tenter de modifier les chmod automatiquement</a>
-                        <a href="index.php" class="btn btn-primary btn-block minecrafter">Relancer la verification</a><br/>
+                        <?php }
+                        }
+
+                        if (isset($return['chmodDossier'])) {
+                            for ($i = 0; $i < count($return['chmodDossier']); $i++) { ?>
+                                <tr>
+                                    <td><?php echo $return['dirDossier'][$i]; ?></td>
+                                    <td><?php echo $return['chmodDossier'][$i]; ?></td>
+                                    <td>777</td>
+                                </tr>
+                            <?php }
+                        }
+
+                        if (isset($return['chmod'])) {
+                            for ($i = 0; $i < count($return['chmod']); $i++) { ?>
+                                <tr>
+                                    <td><?php echo $return['dir'][$i]; ?></td>
+                                    <td><?php echo $return['chmod'][$i]; ?></td>
+                                    <td>755</td>
+                                </tr>
+                            <?php }
+                        } ?>
+                        </tbody>
+                    </table>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <a onclick="ajax_chmod()" class="btn btn-primary text-white btn-block minecrafter">Tenter de
+                                modifier les chmod automatiquement</a>
+                            <a href="index.php" class="btn btn-primary btn-block minecrafter">Relancer la
+                                verification</a><br/>
+                        </div>
                     </div>
                 </div>
             </div>
+
         </div>
 
-     </div>
 
-
-<?php }
+    <?php }
 } ?>
