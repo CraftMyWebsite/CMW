9f9a57d2087449f76b4e26289ede3a5b08587c6b
Thomas aka MrFlamme26 <17986639+thomasmrflamme26@users.noreply.github.com>
2021-10-01 15:29:58 +0200

    [FIX] Email public/privée lors de la création.
    Résolution du bug remonté par les utilisateurs concernant le problème des emails publiques/privés lors de la création d'un compte sur le site.

--- controleur/joueur/inscription.php
+++ controleur/joueur/inscription.php
@@ -13,7 +13,7 @@ if(isset($_POST['pseudo']) AND isset($_POST['mdp']) AND isset($_POST['mdpConfirm
 			$_POST['age'] = (int) htmlspecialchars($_POST['age']);
 			if($_POST["age"] > 9999 || $_POST["age"] < 0) $_POST["age"] = 0;
 
-			$_POST["show_email"] = !empty($_POST['show_email']) ? false : true;
+			$_POST["show_email"] = !empty($_POST['show_email']) ? true : false;
 			$get_Lien = 'http://'.$_SERVER['HTTP_HOST'].'/index.php?&action=validationMail&pseudo='.urlencode($get_Pseudo).'&cle='.urldecode($get_CleUnique).'';
 
 			if (filter_var(get_client_ip_env(), FILTER_VALIDATE_IP)){
