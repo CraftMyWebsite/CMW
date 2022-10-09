f0c3f223814dbff6ef70fbdb7e153f33fe9c84cb
Teyir <60007549+Teyir@users.noreply.github.com>
2022-10-09 12:43:39 +0200

    typo

--- theme/default/entete.php
+++ theme/default/entete.php
@@ -139,7 +139,7 @@
                                             <span class="badge badge-danger">Hors-Ligne</span>
                                         <?php elseif ($_Serveur_['General']['statut'] == 1 && $servEnLigne == true) : ?>
                                             <span class="badge badge-success">En Ligne</span>
-                                            <div class="card-text">Nombres de Joueurs : <strong><?= $playeronline ?></strong>/<?= $maxPlayers; ?></div>
+                                            <div class="card-text">Nombre de Joueurs : <strong><?= $playeronline ?></strong>/<?= $maxPlayers; ?></div>
                                         <?php else : ?>
                                             <span class="badge badge-warning">En Maintenance</span>
                                         <?php endif; ?>
