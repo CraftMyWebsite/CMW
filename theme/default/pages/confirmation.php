3bb8e142ab1847f5d70c632294db7fd3d6e52161
Léo <69589034+Zomblard@users.noreply.github.com>
2022-10-18 17:05:47 +0200

    Hide input

--- theme/default/pages/confirmation.php
+++ theme/default/pages/confirmation.php
@@ -164,7 +164,7 @@
 
                                             <div class="card-body">
 
-                                                <input type="" name="id_topic" value="<?= $id; ?>" />
+                                                <input type="hidden" name="id_topic" value="<?= $id; ?>" />
                                                 <div class="form-group">
                                                     <label for="reason">Indiquez une raison</label>
                                                     <input type="text" class="form-control" name="reason" id="reason" placeholder="Indiquez une raison" required />
